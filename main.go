33c06022e075efde81b84710f58d46edb30ab78f
James Raden <james.raden@gmail.com>
2022-03-23 23:59:29 -0400

    Reset files to d7c1547 (Use request ID middleware, 2022-03-23)

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -41,15 +40,13 @@ func main() {
 	}()
 
 	// Wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer stop()
-
-	<-ctx.Done()
-	ctxShutdownTimeout, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	if err := e.Shutdown(ctxShutdownTimeout); err != nil {
+	if err := e.Shutdown(ctx); err != nil {
 		e.Logger.Fatal(err)
 	}
-	fmt.Println(ctx.Err()) // prints "context canceled"
-	stop()
 }
